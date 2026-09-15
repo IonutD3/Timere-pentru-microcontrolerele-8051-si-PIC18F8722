612240e2fdc6700fdea5814e48cef0e9d662e00c
IonutD3 <146918885+IonutD3@users.noreply.github.com>
2026-09-15 18:04:12 +0300

    Add files via upload

--- 8051/02-timer-interrupt/src/main.c
+++ 8051/02-timer-interrupt/src/main.c
@@ -1,1 +1,32 @@
+/*
+ * 8051 - Timer0 cu intrerupere
+ *
+ * Spre deosebire de exemplul polling, CPU nu mai asteapta activ
+ * overflow-ul timerului; ISR-ul se executa automat.
+ */
+#include <reg51.h>
+#include <stdio.h>
 
+sbit LED = P1^0;
+int counter = 0;
+
+void timer0_isr() interrupt 1{ 
+// Reincarcam Timer0 la fiecare overflow.
+TH0 = 0xFC; 
+TL0 = 0x18; 
+counter++;  
+
+if(counter  == 1000){
+LED =~ LED;   
+counter = 0;}    
+}
+
+void main(){
+    TMOD = 0x01;      
+    TR0 = 1;     // Porneste Timer0.      
+    ET0 = 1;    // Permite intreruperea Timer0.       
+    EA = 1;     // Permite intreruperile globale.
+      
+    while(1)
+    {} // Bucla principala ramane libera.
+}
